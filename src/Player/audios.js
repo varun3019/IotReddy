663838f7a873469b8343c525aba7effb0da4b46d
varun3019 <varunrao1000@gmail.com>
2024-02-05 12:59:05 +0530

    'firstcommit'

--- src/Player/audios.js
+++ src/Player/audios.js
@@ -1,14 +1,14 @@
 exports.songsdata = [
     {
         "title": "Drake - Forever",
-        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+        "url": "https://s3://myawsbucketvarun/Laal Peeli Akhiyaan (Song) Shahid Kapoor,Kriti Sanon,Tanishk,Romy  Teri Baaton Mein Aisa Uljha Jiya.mp3"
     },
     {
         "title": "Linking Park - In the end",
-        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+        "url": "https://s3://myawsbucketvarun/Laal Peeli Akhiyaan (Song) Shahid Kapoor,Kriti Sanon,Tanishk,Romy  Teri Baaton Mein Aisa Uljha Jiya.mp3"
     },
     {
         "title": "Travis Scott - Stop trina be God",
-        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+        "url": "https://s3://myawsbucketvarun/Laal Peeli Akhiyaan (Song) Shahid Kapoor,Kriti Sanon,Tanishk,Romy  Teri Baaton Mein Aisa Uljha Jiya.mp3"
     }
 ]
